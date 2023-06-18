f8ec38b51d532361c80e2f0f0bf130b930514c5e
HastagKc <chaudharykshittiz950@gmail.com>
2023-06-18 07:17:41 +0545

    prime number

--- basicProgramming/dart/ifElse/p7.dart
+++ basicProgramming/dart/ifElse/p7.dart
@@ -1,1 +1,54 @@
-void main() {}
+import 'dart:io';
+import 'dart:math';
+
+bool firstApproach(int num, bool isPrime) {
+  if (num < 2) {
+    isPrime = false;
+  } else {
+    for (int i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  return isPrime;
+}
+
+bool secondApproach(int num, bool isPrime) {
+  /* 
+  Note that if a number has a factor greater than its square root, 
+  it must also have a corresponding factor smaller than the square 
+  root. So checking up to the square root is sufficient.
+  */
+  if (num < 2) {
+    isPrime = false;
+  } else {
+    int sqrtNum = sqrt(num).toInt();
+    for (int i = 2; i <= sqrtNum; i++) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  return isPrime;
+}
+
+void main() {
+  stdout.write('Enter any number: \n');
+  int num = int.parse(stdin.readLineSync()!);
+  bool isPrime = true;
+
+  if (firstApproach(num, isPrime)) {
+    print('$num is a prime number');
+  } else {
+    print('$num is not a prime number');
+  }
+
+  if (secondApproach(num, isPrime)) {
+    print('$num is a prime number');
+  } else {
+    print('$num is not a prime number');
+  }
+}
