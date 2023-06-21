76ba6e5fa398fde07432dbb0631c6b2cafdb584d
HastagKc <chaudharykshittiz950@gmail.com>
2023-06-21 23:12:06 +0545

    reverse string

--- basicProgramming/dart/String/p1.dart
+++ basicProgramming/dart/String/p1.dart
@@ -1,3 +1,21 @@
+import 'dart:io';
+
 void main() {
   //* Reverse String: Reverse the characters in a string.
+
+  stdout.write('Enter your name ?');
+  String name = stdin.readLineSync()!;
+
+  print(name.length);
+
+  String revString = '';
+  for (int i = name.length - 1; i >= 0; i--) {
+    revString = revString + name[i];
+  }
+
+  print(revString.trim());
+
+  // using function
+  String revAgain = revString.split('').reversed.join('');
+  print(revAgain);
 }
